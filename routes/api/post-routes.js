7a17d4283fa1baf539c91bce44a6f0f19d66ad58
Sparkle Party <lexi.slovik@gmail.com>
2022-01-30 20:02:31 -0800

    add comment functionality

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 router.get('/', (req,res) => {
     console.log('========================');
@@ -15,10 +15,19 @@ router.get('/', (req,res) => {
         ],
         order: [['created_at', 'DESC']],
         include: [
-            {
-                model: User,
-                attributes: ['username']
+          // include the Comment model
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
             }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
         ]
     })
       .then(dbPostData => res.json(dbPostData))
@@ -42,6 +51,15 @@ router.get('/:id', (req, res) => {
         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
       ],
       include: [
+        // include the Comment model
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
         {
           model: User,
           attributes: ['username']
